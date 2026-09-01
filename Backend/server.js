2dfb908416366cb11d7b2b6e416ad17be5a18da3
Candela31 <102998114+Candela31@users.noreply.github.com>
2026-09-01 15:39:42 -0300

    Clase 15: DELETE, middleware, router, controllers y data

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
-
-const alumnos = [
+app.use(express.json())
+const alumnosRouter = require("./routers/alumnos.routers")
+app.use("/alumnos.html", alumnosRouter)
+let alumnos = [
     {
         id: 1,
         nombre: "Ana",
@@ -24,6 +26,21 @@ app.get("/alumnos/:id", (req, res) => {
     res.json(alumno)
 })
 
+app.post("/alumnos", (req, res) => {
+    const nuevoAlumno = req.body
+    alumnos.push(nuevoAlumno)
+    res.json({mensaje: "Alumno registrado correctamente"})
+})
+
+app.put("/alumnos/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const alumno = alumnos.find(alumno => alumno.id === id)
+    alumno.id = req.body.id
+    alumno.nombre = req.body.nombre
+    alumno.carrera = req.body.carrera
+    res.json({mensaje: "Alumno actualizado correctamente"})
+})
+
 app.listen(3000, () => {
     console.log("Servidor funcionando en http://localhost:3000")
-}) 
+})
